b680633a270387108e8a97a6fe40ec4fb9d21d2f
Victor Codocedo <kyori@MacBook-Pro-de-Victor.local>
2017-05-26 17:42:34 -0400

    added copyleft notice

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
+"""
+FCA - Python libraries to support FCA tasks
+Copyright (C) 2017  Victor Codocedo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from distutils.core import setup
 setup(
   name = 'fca',
-  packages = ['fca', 'fca.algorithms', 'fca.algorithms.addIntent', 'fca.defs', 'fca.reader'], # this must be the same as the name above
+  packages = ['fca', 'fca.algorithms', 'fca.algorithms.addIntent', 'fca.defs', 'fca.reader'],
   version = '0.2',
   description = 'A library to implement FCA tools',
   author = 'Victor Codocedo',
